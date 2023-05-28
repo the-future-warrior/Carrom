0d657f18eec9ae0e5d0c1aa0ac20ce7c03083a4a
the_mr17 <saptakmanna100@gmail.com>
2023-05-28 19:54:38 +0530

    Implements score system

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private List<Collider2D> pocketList;
+    [SerializeField] private TMP_Text player1ScoreText;
+    [SerializeField] private TMP_Text player2ScoreText;
+
+    private enum Player {
+        Player1,
+        Player2,
+    }
+
+    private int whitePoints = 1;
+    private int blackPoints = 1;
+    private int queenPoints = 2;
+
+    private Player activePlayer = Player.Player1;
+
+    private List<int> scoresList = new List<int>();
+
+    private void Awake() {
+        scoresList.Add(0);
+        scoresList.Add(0);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(Collider2D pocket in pocketList) {
+            pocket.GetComponent<Pockets>().OnPocketTriggered += OnPiecePocketed;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach(Collider2D pocket in pocketList) {
-            
+        
+    }
+
+    private void OnPiecePocketed(object sender, Pockets.OnPocketTriggeredEventArgs e)
+    {
+        GameObject piece = e.piece;
+
+        switch(e.piece.tag) {
+            case "White" :
+                UpdateScore(piece, whitePoints);
+                break;
+
+            case "Black" :
+                UpdateScore(piece, blackPoints);
+                break;
+
+            case "Queen" :
+                UpdateScore(piece, queenPoints);
+                break;
         }
+    }
+
+    private void UpdateScore(GameObject piece, int points) {
+        switch (activePlayer) {
+                    case Player.Player1:
+                        scoresList[0] += points;
+                        player1ScoreText.text = scoresList[0].ToString();
+                        break;
+                    case Player.Player2:
+                        scoresList[1] += points;
+                        player2ScoreText.text = scoresList[1].ToString();
+                        break;
+                }
+                Destroy(piece);
     }
 }
--- Assets/Scripts/Pockets.cs
+++ Assets/Scripts/Pockets.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Pockets : MonoBehaviour
 {
+    public event EventHandler<OnPocketTriggeredEventArgs> OnPocketTriggered;
+    public class OnPocketTriggeredEventArgs : EventArgs {
+        public GameObject piece;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +23,8 @@ public class Pockets : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D collider) {
-        collider.gameObject.SetActive(false);
+        OnPocketTriggered?.Invoke(this, new OnPocketTriggeredEventArgs {
+            piece = collider.gameObject
+        });
     }
 }
